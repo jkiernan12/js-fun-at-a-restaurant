739b7072f30ca60b83c3b291703c95f44efe1232
John Kiernan <jkiernan12@yahoo.com>
2021-08-25 17:41:57 -0400

    Add meal.js and warm-up.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  var result = "Delicious " + name;
+  return result;
+}
+function createMenuItem(itemName, itemPrice, itemType) {
+  menuItem = {
+    name: itemName,
+    price: itemPrice,
+    type: itemType
+  }
+  return menuItem;
+}
+function addIngredients(ingredient, arr) {
+  if (arr.indexOf(ingredient) === -1){
+    arr = arr.push(ingredient);
+  }
 
+  return arr;
 }
 
+function formatPrice(price) {
+  return "$" + price;
+}
 
+function decreasePrice(price) {
+  return price * .9;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipeObject = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return recipeObject;
+}
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+   formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
