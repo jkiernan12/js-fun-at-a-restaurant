f6b1d7ccfc636f26e0104f79496aef67b5b0626b
John Kiernan <jkiernan12@yahoo.com>
2021-08-26 13:49:07 -0400

    Add restaurant.js and restaurant-test.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,46 @@
+function createRestaurant(restaurantName) {
+  var restaurant = {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
 
+   return restaurant;
+}
+
+function addMenuItem(restaurant, menuItem) {
+  var restaurantMenuType = restaurant.menus[menuItem.type];
+  var isNotOnMenu = true;
+
+  for (let i = 0; i < restaurantMenuType.length; i++) {
+    if (restaurantMenuType[i] === menuItem) {
+      isNotOnMenu = false;
+      }
+    }
+
+  if (isNotOnMenu) {
+      restaurantMenuType.push(menuItem);
+  }
+}
+
+function removeMenuItem(restaurant, menuItemName, menuItemType) {
+  var restaurantItemType = restaurant.menus[menuItemType];
+
+    for (var i = 0; i < restaurantItemType.length; i++) {
+      if (restaurantItemType[i].name === menuItemName) {
+        restaurantItemType.splice(i, 1);
+        return `No one is eating our ${menuItemName} - it has been removed from the ${menuItemType} menu!`
+      }
+    }
+
+      return `Sorry, we don't sell ${menuItemName}, try adding a new recipe!`;
+  }
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+   createRestaurant,
+   addMenuItem,
+   removeMenuItem
 }
