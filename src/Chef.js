d519ed5751e0b202a1bb8fbc26c8096188fb9618
John Kiernan <jkiernan12@yahoo.com>
2021-08-26 14:11:12 -0400

    Add Chef.js and Chef-test.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,26 @@
 class Chef {
-  constructor() {
-    
+  constructor(chefName, chefRestaurant) {
+    this.name = chefName;
+    this.restaurant = chefRestaurant;
+  }
+  greetCustomer(customerName, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${customerName}!`
+    } else {
+      return `Hello, ${customerName}!`
+    }
+  }
+  checkForFood(foodItem) {
+    var currentMenuArray = this.restaurant.menus[foodItem.type];
+    for (var i = 0; i < currentMenuArray.length; i++) {
+      if (currentMenuArray[i] === foodItem) {
+        return `Yes, we're serving ${foodItem.name} today!`
+      }
+    }
+    return `Sorry, we aren't serving ${foodItem.name} today.`
   }
 }
+
+
 
 module.exports = Chef;
