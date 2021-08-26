efaf2e2a033945434db673d1338d27da0202480b
John Kiernan <jkiernan12@yahoo.com>
2021-08-25 21:27:05 -0400

    Add order.js with tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,46 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+      deliveryOrders.push(order);
+    }
+  return deliveryOrders;
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (let i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      deliveryOrders.splice(i, 1);
+      break;
+    }
+  }
+  return deliveryOrders;
+}
+
+function listItems(deliveryOrders) {
+  var orderNames = "";
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (i !== (deliveryOrders.length - 1)) {
+    orderNames = orderNames + deliveryOrders[i].item + ", ";
+    } else {
+    orderNames = orderNames + deliveryOrders[i].item;
+  }
+}
+  return orderNames;
+}
+
+function searchOrder(deliveryOrders, item) {
+  var isItemPresent = false;
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === item) {
+      isItemPresent = true;
+      break;
+    }
+  }
+  return isItemPresent;
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
